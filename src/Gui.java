bd8f49de8e1e98c0689050afed5e3017bc65dcab
Eric <ericmathias2012@gmail.com>
2023-08-25 18:38:24 -0300

    Atualização GUI

--- src/Gui.java
+++ src/Gui.java
@@ -1,6 +1,9 @@
 package src;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -14,16 +17,46 @@ public class Gui extends JFrame{
         this.setTitle("Music from text");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        layout = new BorderLayout(5,5);	//ESPAÇOS DE 5 PIXELS
-		this.setLayout(layout);
-
-        JButton button = new JButton("Open file");
-        button.setPreferredSize(new Dimension(40, 40));
-        button.addActionListener(e -> {
+        layout = new BorderLayout(25,25);	//ESPAÇOS DE 25 PIXELS
+        this.setLayout(layout);
+		
+        JButton openFileButton = new JButton("Open file");
+        openFileButton.setPreferredSize(new Dimension(130, 40));
+        
+        JButton startButton = new JButton("Convert to music");
+        startButton.setPreferredSize(new Dimension (130,40));
+        
+       
+        openFileButton.addActionListener(e -> {
             selectFile();
         });
+        
+        JTextField userText = new JTextField(30);
+        
+        
+        JPanel buttonPanel = new JPanel(new FlowLayout());
+        buttonPanel.add(openFileButton);
+        buttonPanel.add(startButton);
+        
+        BufferedImage originalImage = null;
+        try {
+            File imageFile = new File("img/img.png"); 
+            originalImage = ImageIO.read(imageFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        this.add(button, BorderLayout.CENTER);
+        int newWidth = 175; // Novo tamanho da largura da imagem
+        int newHeight = 175; // Novo tamanho da altura da imagem
+        Image resizedImage = originalImage.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
+
+        // Exibir a imagem redimensionada
+        ImageIcon imageIcon = new ImageIcon(resizedImage);
+        JLabel imageLabel = new JLabel(imageIcon);
+        add(imageLabel, BorderLayout.NORTH);
+        
+        this.add(userText, BorderLayout.CENTER);
+        this.add(buttonPanel, BorderLayout.SOUTH);
 
         this.setLocationRelativeTo(null);
         this.setSize(400, 600);
@@ -36,10 +69,12 @@ public class Gui extends JFrame{
     } 
 
     public void selectFile() {
-        JFileChooser chooser = new JFileChooser();
+    	JFileChooser chooser = new JFileChooser();
         // optionally set chooser options ...
         if (chooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
             File f = chooser.getSelectedFile();
+            InputReader reader = new InputReader(f);
+            System.out.println(reader.getNextChar());
             
             // read  and/or display the file somehow. ....
         } else {
